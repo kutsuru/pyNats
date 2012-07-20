f56b5a79db463a5e2336fd52ba6b923f6cfa4a73
kutsuru <kutsuru@gmail.com>
2012-07-20 13:04:02 +0200

    speed up the density rendering

--- src/rendering.py
+++ src/rendering.py
@@ -12,6 +12,10 @@ from OpenGL.GL import *
 import math
 from navier_strokes_orig import NavierStrokesOrig
 from navier_strokes import NavierStrokes
+from OpenGL.arrays import vbo
+import OpenGL
+from OpenGL.GL import shaders
+import time
 
 class LazyNavier(object):
     def __init__(self, main, size=5, dt=.1, diff=0., visc=0., force=5.,
@@ -38,8 +42,8 @@ class LazyNavier(object):
             dt = self.dt
                               
         self.me.density_step(self.me.density, self.me.density_prev,
-                             self.me.u, self.me.v, self.diff, dt)
-
+                             self.me.u, self.me.v, self.diff, dt)      
+        
 class MatrixDensity(object):
     def __init__(self, matrix, x, y, width, height):
         self._matrix = matrix
@@ -48,31 +52,54 @@ class MatrixDensity(object):
         self._width = width
         self._height = height
         
+        self._matrix_width = matrix.shape[0]
+        
         self._case_width = width / float(matrix.shape[0])
         self._case_height = height / float(matrix.shape[1])
-    
-    def render(self):
-        w = self._case_width
-        h = self._case_height
         
+        # Generate the vertex and color array for opengl
+        nb_squares = matrix.size
+        nb_triangles = nb_squares * 2
+        self._vertex = np.zeros((nb_triangles * 3, 2)) # 3 vert per triangle
+        
+        x, y = 0, 0
+        step_x = self._case_width
+        step_y = self._case_height
+        
+        for i in range(nb_squares):
+            xx = i % matrix.shape[0]
+            yy = i / matrix.shape[0]
+            
+            x, y = xx  * step_x, yy * step_y
+            self._vertex[i * 6,:] = [x, y]
+            self._vertex[i * 6 + 1,:] = [x + step_x, y]
+            self._vertex[i * 6 + 2,:] = [x, y + step_y]
+            self._vertex[i * 6 + 3,:] = [x + step_x, y]
+            self._vertex[i * 6 + 4,:] = [x + step_x, y + step_y]
+            self._vertex[i * 6 + 5,:] = [x, y + step_y]
+            
+        self._colors = np.zeros((nb_triangles * 3, 2)) # 1 color per vertex
+        self._colors = np.random.rand(nb_triangles * 3, 3)
+        
+        self._nb_squares = nb_squares
+        self._nb_vertex = nb_triangles * 3
+        
+    def render(self):
+        # Update color array
         it = np.nditer(self._matrix, flags=['multi_index'])
         while not it.finished:
             value, (X, Y) = it[0], it.multi_index
-            x = self._x + X * w
-            y = self._y + Y * h
-
-            glBegin(GL_QUADS)
-            glColor3f(value, value, value)
-            glVertex2f(x, y)
-            glColor3f(value, value, value)
-            glVertex2f(x + w, y)
-            glColor3f(value, value, value)
-            glVertex2f(x + w, y + h)
-            glColor3f(value, value, value)
-            glVertex2f(x, y + h)
-            glEnd()
+            i = 6 * (Y * self._matrix.shape[0] + X)
+            self._colors[i:i+6,:] = np.array((value, value, value))
             
             it.iternext()
+        
+        # Render vertex
+        glVertexPointerd(self._vertex)
+        glColorPointerd(self._colors)
+        glEnableClientState(GL_VERTEX_ARRAY)
+        glEnableClientState(GL_COLOR_ARRAY)
+        glDrawArrays(GL_TRIANGLES, 0, self._nb_vertex)
             
     def click_at(self, x, y):
         X = int(math.floor((x - self._x) / self._case_width))
@@ -80,7 +107,11 @@ class MatrixDensity(object):
              + self._matrix.shape[1]
              - int(math.floor((y - self._y) / self._case_height)))
         
-        self._matrix[X, Y] = 0.5
+        self._matrix[X, Y] = 0.9
+        self._matrix[X + 1, Y] = 0.9
+        self._matrix[X - 1, Y] = 0.9
+        self._matrix[X, Y + 1] = 0.9
+        self._matrix[X, Y - 1] = 0.9
             
 class MatrixVectors(object):
     def __init__(self, u, v, x, y, width, height):
@@ -137,19 +168,24 @@ class MatrixVectors(object):
         self._v[X, Y+1] = -REL_Y / 2.0        
         self._u[X, Y-1] = REL_X / 2.0
         self._v[X, Y-1] = -REL_Y / 2.0
-    
-            
-def init():
-    pygame.init()
-    pygame.display.set_mode((640, 480), OPENGL|DOUBLEBUF)
-    
-    glClearColor(1.0, 1.0, 1.0, 0.0)
-    glColor3f(0.0, 0.0, 0.0)
-    glPointSize(4.0)
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    glOrtho (0, 640, 0, 480, -1, 1)
-    
+
+class Renderer(object):
+    """
+    Base class for a renderer (uncomplete)
+    """
+    def init(self):
+        pygame.init()
+        pygame.display.set_mode((640, 480), OPENGL|DOUBLEBUF)
+        
+        glClearColor(1.0, 1.0, 1.0, 0.0)
+        glColor3f(0.0, 0.0, 0.0)
+        glPointSize(4.0)
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        glOrtho (0, 640, 0, 480, -1, 1)
+        
+        #pos_vbo = vbo.VBO(data=pos, usage=GL_DYNAMIC_DRAW, target=GL_ARRAY_BUFFER)
+        
 def update(delta, mdensity, mvectors):
     event = pygame.event.poll ()
     
@@ -180,9 +216,10 @@ def draw(rendered):
 
 
 def main(args):
-    init()
+    renderer = Renderer()
+    renderer.init()
     
-    size = 10
+    size = 30
     nats = LazyNavier(NavierStrokes, size=size)
     #nats = LazyNavier(NavierStrokesOrig, size=size)
     
